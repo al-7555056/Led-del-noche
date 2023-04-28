1bcdcffcce68707d1b975dcee76beddae96b48e0
Haoqi Shan Xu <104140318+al-7555056@users.noreply.github.com>
2023-04-28 09:57:50 +0200

    Es el comienzo

--- main.ts
+++ main.ts
@@ -1,1 +1,72 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    if (Interruptor) {
+        basic.showString("OFF")
+        Interruptor = false
+        basic.clearScreen()
+    } else {
+        basic.showString("ON")
+        Interruptor = true
+        if (control2) {
+            basic.showString("CM")
+        } else {
+            basic.showString("LM")
+        }
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    Cambio = false
+    led.setBrightness(255)
+    if (control2) {
+        basic.showString("LM")
+        control2 = false
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showString("CM")
+        control2 = true
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    Cambio = true
+})
+let Interruptor = false
+let Cambio = false
+let control2 = false
+control2 = true
+Cambio = true
+Interruptor = false
+led.setBrightness(255)
+basic.forever(function () {
+    if (Interruptor && Cambio) {
+        if (control2) {
+            if (input.lightLevel() >= 128) {
+                led.setBrightness(255)
+            } else {
+                led.setBrightness(0)
+            }
+        } else {
+            led.setBrightness(input.lightLevel())
+        }
+    } else {
+        led.setBrightness(255)
+    }
+})
